e1f9e436d2637a07d2272a8d2e2a2f512fc2edb0
musapeace <mubarakinda@gmail.com>
2024-12-30 12:55:13 +0000

    weather recommendations

---
+++ weather_advice.py
@@ -0,0 +1,11 @@
+# weather recommendation program 
+weather = input("What's the weather like today?, (sunny/rainy/cold):")
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    weather == "cold"
+    print("Sorry, I don't have recommendations for this weather")
+
+    
